92a7d230ddbf6da4c04a5d97020e184883866b1f
BuildTools <unconfigured@null.spigotmc.org>
2022-09-03 10:10:45 -0500

    Changes by Jacob

--- app/python-levels/16.py
+++ app/python-levels/16.py
@@ -1,2 +1,10 @@
-PlayerHand
-Dealerh
+Player1Hand = ['Ace', 'King']
+Player1Score = 0
+Player2Hand = ['Two', 'Two']
+Player2Score = 0
+Dealer = ['Queen', 'Jack', 'Ten']
+CardWeight = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two']
+for card in range(0, len(Player1Hand)):
+    for count in range(0, 13):
+        if Player1Hand[card] == CardWeight[count]:
+            Player1Score += count
