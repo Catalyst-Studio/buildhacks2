38b8b9f23a05bfd6a88651e7d0ed88904abb8ba9
BuildTools <unconfigured@null.spigotmc.org>
2022-09-03 10:31:24 -0500

    Changes by Jacob

--- app/python-levels/16.py
+++ app/python-levels/16.py
@@ -1,13 +1,19 @@
-Player1Hand = ['Ace', 'King']
+Player1Hand = ['Seven', 'Nine']
 Player1Score = 0
-Player2Hand = ['Two', 'Two']
-Player2Score = 0
-CardWeight = [ 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack','Queen', 'King', 'Ace']
+Player2Hand = ('Three', 'King')
+Player2Score == 0
+CardWeight = ['Ace', 'Two', 'Three' 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack','Queen', 'King']
 for card in range(0, 2):
     for count in range(0, 13):
         if Player1Hand[card] == CardWeight[count]:
-            Player1Score += count
-            print(Player1Score)
+            Player1Score += count + 1
         if Player2Hand[card] == CardWeight[count]:
-            Player2Score += count
-            print(Player2Score)
+            Player2Score += count + 1
+
+    if Player1Hand[card] == 'King':
+        if Player2Hand[card] == 'Ace':
+            Player2Score += 13
+    if Player2Hand[cards] == 'King':
+        if Player1Hand[card] = 'Ace':
+            Player1Score =+ 13
+print("Player 1 Score: " Player1Score " vs " "Player 2 Score: ", Player2Score)
